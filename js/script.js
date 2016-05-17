39889706d34e3df277ed0bfc98b324a192d6c3a3
galdinorosas <dino.rosas@gmail.com>
2016-05-17 15:41:52 -0700

    corrections to people page.

--- js/script.js
+++ js/script.js
@@ -3,20 +3,32 @@ $(document).ready(function() {
     $(".teamBlount").click(function(event) {
         $('html, body').animate({
             scrollTop: $(".scroll").offset().top
-        }, 1500);
+        }, 800);
 
         switch (event.currentTarget.className) {
             case "blountLink teamBlount img-rounded":
-                $(".dicksonInfo, .dundasInfo").css('display', "none");
+                $(".dicksonInfo, .dundasInfo, .smithInfo, .vasuInfo, .valerieInfo").css('display', "none");
                 $(".blountInfo").fadeIn(1000);
                 break;
             case "dicksonLink teamBlount img-rounded":
-                $(".blountInfo, .dundasInfo").css('display', "none");
+                $(".blountInfo, .dundasInfo, .smithInfo, .vasuInfo, .valerieInfo").css('display', "none");
                 $(".dicksonInfo").fadeIn(1000);
                 break;
             case "dundasLink teamBlount img-rounded":
-                $(".blountInfo, .dicksonInfo").css('display', "none");
+                $(".blountInfo, .dicksonInfo, .smithInfo, .vasuInfo, .valerieInfo").css('display', "none");
                 $(".dundasInfo").fadeIn(1000);
+                break;
+            case "smithLink teamBlount img-rounded":
+                $(".blountInfo, .dicksonInfo, .dundasInfo, .vasuInfo, .valerieInfo").css('display', "none");
+                $(".smithInfo").fadeIn(1000);
+                break;
+            case "vasuLink teamBlount img-rounded":
+                $(".blountInfo, .dicksonInfo, .dundasInfo, .smithInfo, .valerieInfo").css('display', "none");
+                $(".vasuInfo").fadeIn(1000);
+                break;
+            case "valerieLink teamBlount img-rounded":
+                $(".blountInfo, .dicksonInfo, .dundasInfo, .smithInfo, .vasuInfo").css('display', "none");
+                $(".valerieInfo").fadeIn(1000);
                 break;
         }
     });
